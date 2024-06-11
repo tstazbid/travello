ce8ec6f3a1c6572ff153ab5500e383c05d3ea0d9
tstazbid <tazbidus15-13711@diu.edu.bd>
2024-06-11 13:14:35 +0600

    first

---
+++ calc/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request) :
+    return render(request, 'home.html', {'name' : 'Tazbid'})
+
+
+def add(request) :
+
+    num1 = int(request.POST['number1'])
+    num2 = int(request.POST['number2'])
+
+    res = num1 + num2
+    return render(request, 'result.html', {'result' : res})
