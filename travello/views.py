7c853386254264b9fc0b513a867b5255eb1b578a
tstazbid <tazbidus15-13711@diu.edu.bd>
2024-06-11 17:07:05 +0600

    dynamic data pass

--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from . models import Destination
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    dest1 = Destination()
+    dest1.name = "Dhaka"
+    dest1.desc = "Popular city"
+    dest1.img = "destination_1.jpg"
+    dest1.price = 550
+
+    dest2 = Destination()
+    dest2.name = "Indonesia"
+    dest2.desc = "Chilling area everywhere"
+    dest2.img = "destination_2.jpg"
+    dest2.price = 650
+
+    dest3 = Destination()
+    dest3.name = "San Francisco"
+    dest3.desc = "Explore American culture"
+    dest3.img = "destination_3.jpg"
+    dest3.price = 700
+
+    dests = [dest1, dest2, dest3]
+    return render(request, "index.html", {'dests' : dests})
