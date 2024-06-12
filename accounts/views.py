458528ca0c183643c6641788d599aabb21ab9a57
tstazbid <tazbidus15-13711@diu.edu.bd>
2024-06-12 18:46:04 +0600

    validation check msgs show

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -16,16 +17,19 @@ def register(request):
 
         if password1 == password2:
             if User.objects.filter(username = username).exists():
-                print('User name already taken')
+                messages.info(request, 'User name already taken')
+                return redirect('register')
             elif User.objects.filter(email = email).exists():
-                print('Email alredy used!')
+                messages.info(request, 'Email alredy used!')
+                return redirect('register')
             else:
                 user = User.objects.create_user(username = username, password=password1, email=email, first_name=first_name, last_name = last_name)
                 user.save()
                 print('User created')
+                return redirect('/')
         else:
-            print('Password didnot match')
-        return redirect('/')
+            messages.info(request, 'Password didnot match')
+            return redirect('register')
     else:
         return render(request, 'register.html')
 
